0d3cf1f4a788d09641d347702644693306ee6fc7
Cedric-Yaba-In <c.nguendap@yabain.com>
2023-05-24 22:49:49 +0200

    update url image

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,8 +6,11 @@ export const environment = {
   production: false,
     // url:  'https://api-dev.smartestlotto.io',
     // urlProd:  'https://api-dev.smartestlotto.io'
-    url:  'http://srv.yaba-in.com:3000',
-    urlProd:  'http://srv.yaba-in.com:3000'
+    // url:  'http://srv.yaba-in.com:3000',
+    // urlProd:  'http://srv.yaba-in.com:3000'
+
+    url:  'http://localhost:3000',
+    urlProd:  'http://localhost:3000'
 };
 
 /*

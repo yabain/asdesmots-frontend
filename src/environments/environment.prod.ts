be6a0da34cc1008632e6a3e53fa6fa84064ddd02
Cedric-Yaba-In <c.nguendap@yabain.com>
2023-07-30 21:27:49 +0200

    update backend url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,4 +2,6 @@ export const environment = {
   production: true,
   url:  'https://asdesmots-api.yaba-in.com',
   urlProd:  'https://asdesmots-api.yaba-in.com'
+  // url:  'http://localhost:3000',
+  // urlProd:  'http://localhost:3000'
 };

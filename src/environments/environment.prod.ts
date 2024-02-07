07be723161b97105e24a569cbdc5d1121386d75d
Gabriel SIGNE <gibrilekoudjou@gmail.com>
2024-02-07 13:08:06 +0100

    Aucune modification juste la reparation des bugs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  // url:  'https://asdesmots-api.yaba-in.com',
-  // urlProd:  'https://asdesmots-api.yaba-in.com'
+  url:  'https://asdesmots-api.yaba-in.com',
+  urlProd:  'https://asdesmots-api.yaba-in.com'
   // url:  'http://localhost:3000',
   // urlProd:  'http://localhost:3000'
-  url:  'http://192.168.167.103:3000',
-  urlProd:  'http://192.168.167.103:3000'
+  // url:  'http://192.168.167.103:3000',
+  // urlProd:  'http://192.168.167.103:3000'
 };

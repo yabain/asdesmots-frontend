4e6d7b021e727e3551937568d7decea924d58ef6
valdoblair <tchindavaldoblair@gmail.com>
2024-10-10 11:05:32 +0100

    Modification interface interface suppression de role

--- src/app/back-office/all-modules/role/role-users/role-users.component.ts
+++ src/app/back-office/all-modules/role/role-users/role-users.component.ts
@@ -10,101 +10,118 @@ import { log } from 'console';
 
 
 
-@Component({
-  selector: 'app-role-users',
-  templateUrl: './role-users.component.html',
-  styleUrls: ['./role-users.component.css']
-})
-export class RoleUsersComponent implements OnInit, AfterViewInit {
+@Component( {
+	selector: 'app-role-users',
+	templateUrl: './role-users.component.html',
+	styleUrls: [ './role-users.component.css' ]
+} )
+export class RoleUsersComponent implements OnInit, AfterViewInit
+{
 
 
-  id : string = '';
-  userData: User = new User();
-  roleChooseName: any;
-  controlModal: boolean = true;
-  removeDone: boolean = false;
-  waitingResponse: boolean = false;
-
-
-
-  constructor(private route: ActivatedRoute,
-              public roleService: RoleService,
-              private router: Router,
-              private location: Location,
-              private toastr: ToastrService,
-              private translate: TranslateService,
-              private translation: TranslationService,
-              private cdRef:ChangeDetectorRef
-              ) {
-                this.initTranslation();
-              }
-
-  ngOnInit(): void {
-
-  }
-
-  ngAfterViewInit() {
-    this.getIDRole();
-    this.loadListUsers();
-    this.startListener();
-    this.cdRef.detectChanges();
-    this.getRoleName()
-  }
+	id: string = '';
+	userData: User = new User();
+	roleChooseName: any;
+	controlModal: boolean = true;
+	removeDone: boolean = false;
+	waitingResponse: boolean = false;
 
 
 
-  getIDRole(){
-    this.id = this.route.snapshot.params['idrole'];
-  }
+	constructor ( private route: ActivatedRoute,
+		public roleService: RoleService,
+		private router: Router,
+		private location: Location,
+		private toastr: ToastrService,
+		private translate: TranslateService,
+		private translation: TranslationService,
+		private cdRef: ChangeDetectorRef
+	)
+	{
+		this.initTranslation();
+	}
 
-  initTranslation(){
-      this.translate.use(this.translation.getCurrentLanguage());
-  }
-  getRoleName(){
-   this.roleChooseName = this.roleService.getNameOfRole(this.id);
-   return this.roleChooseName
-   
-  }
+	ngOnInit(): void
+	{
 
-  startListener(){
-      this.router.events.subscribe((event)=>{
-          if(event instanceof NavigationStart){
-              this.getIDRole();
-              this.loadListUsers();
-          }
-      })
-  }
+	}
 
-  doRemove(){
-    this.removeDone = false;
-    this.waitingResponse = true;
-    console.log("id role: ", this.id);
-    const roleId = this.id;
-    const userId = this.userData._id;
-    console.log("user id",this.userData._id);
-      this.roleService.removeRole(roleId, userId).subscribe(
-      (resp) => {
-        // Gérer la réponse du serveur en cas de succès
-        this.waitingResponse = false;
-        const closeButton = document.getElementById('cancel-btn2');
-        if (closeButton) {
-        closeButton.click();
-      }
-      this.roleService.getUsersByRoleId(roleId);
-      },
-      (error) => {
-        // Gérer les erreurs du serveur
-        this.waitingResponse = false;
-      }
-    );
-  }
+	ngAfterViewInit()
+	{
+		this.getIDRole();
+		this.getRoleName()
+		this.loadListUsers();
+		this.startListener();
+		this.cdRef.detectChanges();
 
-  loadListUsers(){
-      this.roleService.getUsersByRoleId(this.id);
-  }
+	}
 
-  backClicked(){
-      this.location.back();
-  }
+
+
+	getIDRole()
+	{
+		this.id = this.route.snapshot.params[ 'idrole' ];
+	}
+
+	initTranslation()
+	{
+		this.translate.use( this.translation.getCurrentLanguage() );
+	}
+	getRoleName()
+	{
+		this.roleChooseName = this.roleService.getNameOfRole( this.id );
+		return this.roleChooseName
+
+	}
+
+	startListener()
+	{
+		this.router.events.subscribe( ( event ) =>
+		{
+			if ( event instanceof NavigationStart )
+			{
+				this.getIDRole();
+				this.loadListUsers();
+			}
+		} )
+	}
+
+	doRemove()
+	{
+		this.removeDone = false;
+		this.waitingResponse = true;
+		console.log( "id role: ", this.id );
+		const roleId = this.id;
+		const userId = this.userData._id;
+		console.log( "user id", this.userData._id );
+		this.roleService.removeRole( roleId, userId ).subscribe(
+			( resp ) =>
+			{
+				// Gérer la réponse du serveur en cas de succès
+				this.waitingResponse = false;
+				const closeButton = document.getElementById( 'cancel-btn2' );
+				if ( closeButton )
+				{
+					closeButton.click();
+				}
+				this.roleService.getUsersByRoleId( roleId );
+			},
+			( error ) =>
+			{
+				// Gérer les erreurs du serveur
+				this.waitingResponse = false;
+			}
+		);
+	}
+
+	loadListUsers()
+	{
+		this.roleService.getUsersByRoleId( this.id );
+	}
+
+	backClicked()
+	{
+		this.location.back();
+	}
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,15 +4,18 @@
 
 export const environment = {
   production: false,
-    // url:  'http://localhost:3000',
-    // urlProd:  'http://localhost:3000'
-    // socketIoUrl:  'http://127.0.0.1:3000',
-    url:  'http://192.168.1.122:3000',
-    urlProd:  'http://192.168.1.122:3000',
-    socketIoUrl:  'http://192.168.1.122:3000',
-    // url:  'http://192.168.1.125:3000',
-    // urlProd:  'http://192.168.1.125:3000',
-    // socketIoUrl:  'http://192.168.1.125:3000',
+  // url:  'http://localhost:3000',
+  // urlProd:  'http://localhost:3000'
+  // socketIoUrl:  'http://127.0.0.1:3000',
+  // url: 'http://192.168.43.101:3000',
+  // urlProd: 'http://192.168.43.101:3000',
+  // socketIoUrl: 'http://192.168.43.101:3000',
+  url: 'http://192.168.1.122:3000',
+  urlProd: 'http://192.168.1.122:3000',
+  socketIoUrl: 'http://192.168.1.122:3000',
+  // url:  'http://192.168.1.125:3000',
+  // urlProd:  'http://192.168.1.125:3000',
+  // socketIoUrl:  'http://192.168.1.125:3000',
 
   //   url:  'https://asdesmots-api.yaba-in.com',
   // urlProd:  'https://asdesmots-api.yaba-in.com'
